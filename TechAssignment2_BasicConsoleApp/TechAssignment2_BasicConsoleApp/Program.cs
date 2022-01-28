f7c8518eef9e0b864f79901498e6551ace5e7a62
uliana-tr <98507918+uliana-tr@users.noreply.github.com>
2022-01-27 19:05:45 -0500

    Add files via upload

---
+++ TechAssignment2_BasicConsoleApp/TechAssignment2_BasicConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿//Uliana Tretyakova
+
+using System;
+
+namespace TechAssignment2_BasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "I believe that Winston Churchill's quote is very inspiring: \n" +
+                "\"A pessimist sees the difficulty in every opportunity; an optimist sees the opportunity in every difficulty.\"\n" +
+                "Submitted by Uliana Tretyakova");
+        }
+    }
+}
